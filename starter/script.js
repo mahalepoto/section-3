00bfa489c2b867bf8c05cefcc58148c1bba6d9f1
Mushtaq A Halepoto <mahalepoto@outlook.com>
2019-06-23 23:53:53 +0100

    Section 3: Lecture 39. Hoisting with Function Expression.

--- starter/script.js
+++ starter/script.js
@@ -23,6 +23,25 @@ statements, even before executind the code.*/
 
 // calcAg(1982); // Moving function call up to test Hoisting
 
+calcAge(2008); // Mving call from live 35 to test function expression Hoisting
+// throws error of 'script.js:26 Uncaught ReferenceError: calcAge is not defined'.
+
+/*
+Now it throws error as in function expression only variable are stored not their values.
+So though variable are Hoisted but they are given value of undefinde, untill
+code is executed and variables are given their definde vlaues.*/
+
+
+
+// Example of function expression
+
+var age = function calcAge(birthYear) {
+  console.log(2019 - birthYear);
+}
+
+// function expression call
+// calcAg(2008); // Moving this call to upward to test Hoisting
+
 
 
 
